39b6e653ff82e3c915f8af42c11bb2735c33e820
1215kkm <66404083+1215kkm@users.noreply.github.com>
2024-06-14 18:25:04 +0900

    웹디자인기능사 D-1

---
+++ script/action.js
@@ -0,0 +1,34 @@
+setInterval(function(){
+    $('.slide ul').animate({top:-400}, function(){
+        $('.slide li').eq(0).appendTo($('.slide ul'))
+        $('.slide ul').css({top:0})
+    })
+}, 2000)
+
+/* 공지사항, 갤러리 */
+$('.title_box h2').eq(1).click(function(){
+    $('.gallery').show();
+    $('.notice').hide();
+
+    $('.title_box h2').eq(1).addClass('on')
+    $('.title_box h2').eq(0).removeClass()
+})
+
+$('.title_box h2').eq(0).click(function(){
+    $('.gallery').hide();
+    $('.notice').show();
+
+    $('.title_box h2').eq(0).addClass('on')
+    $('.title_box h2').eq(1).removeClass()
+})
+
+
+
+/* popup */
+$('.notice li').eq(0).click(function(){
+    $('.popup').show()
+})
+
+$('.popup a').click(function(){
+    $('.popup').hide()
+})
